fe6f59fd36e411d5898823d08e0a870fc03949cd
w0617 <364240243@qq.com>
2018-09-08 17:07:07 +0800

    ignore

--- build/CMakeFiles/Mapper_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Mapper_automoc.cpp"
-  "CMakeFiles/Mapper_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Mapper_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
